5f75b94400e623b7e99cc910cac43ea111a9df29
Tuntun Kumar <tuntunkumaritarhi@gmail.com>
2026-06-20 22:28:37 +0530

    shipment-markplace

---
+++ src/main/java/com/ts/shipment_marketplace/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ts.shipment_marketplace.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ts.shipment_marketplace.Entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
